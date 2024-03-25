d15614c8c87b8c5706b173d4e92e86113c1486a2
Jeris Tree <jerislauren20@gmail.com>
2024-03-26 01:23:47 +0800

    Reverted back to development backend

--- frontend/src/store/services/mainApi.jsx
+++ frontend/src/store/services/mainApi.jsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const mainApi = createApi({
     reducerPath: 'main',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://thrifttracer-backend.onrender.com',
+        baseUrl: 'https://localhost:5555',
         prepareHeaders: (headers, { getState }) => {
             const token = getState().user.value.token
             console.log(token);
