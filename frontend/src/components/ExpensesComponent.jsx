af89567917ab2d29f9562a825359cbf3498e8592
Jeris Tree <jerislauren20@gmail.com>
2024-03-28 03:52:27 +0800

    prod test

--- frontend/src/components/ExpensesComponent.jsx
+++ frontend/src/components/ExpensesComponent.jsx
@@ -19,18 +19,18 @@ const ExpensesComponent = ({ transaction, onClickHandler }) => {
     return (
         <div className="p-4 my-2 mx-4 flex flex-col bg-gray-800 rounded-md break-words">
             <div className="flex space-bet justify-between break-words">
-                <h2 className="px-1 font-bold break-words">{transaction.title}</h2>
+                <h2 className="px-1 font-bold break-words">{transaction?.title}</h2>
                 <button className="rounded-xl bg-red-700 px-1 " onClick={handleClick}><MdDelete size={15} /></button>
             </div>
 
-            {transaction.description && <p className="bg-gray-700 rounded-md my-2 px-1">{transaction.description}</p>}
-            <p className="text-orange-600 font-bold px-1">₱{transaction.amount}</p>
-            {transaction.category ? <><p className="px-1">Category: {transaction.category}</p>
-                <p className="px-1">Account: {transaction.account}</p> </>
+            {transaction.description && <p className="bg-gray-700 rounded-md my-2 px-1">{transaction?.description}</p>}
+            <p className="text-orange-600 font-bold px-1">₱{transaction?.amount}</p>
+            {transaction.category ? <><p className="px-1">Category: {transaction?.category}</p>
+                <p className="px-1">Account: {transaction?.account}</p> </>
                 :
-                <p className="px-1">Account: {transaction.account}</p>}
+                <p className="px-1">Account: {transaction?.account}</p>}
 
-            <p className="px-1">{format(new Date(transaction.createdAt), 'MMM d, y | p')}</p>
+            <p className="px-1">{format(new Date(transaction?.createdAt), 'MMM d, y | p')}</p>
 
         </div>
 
